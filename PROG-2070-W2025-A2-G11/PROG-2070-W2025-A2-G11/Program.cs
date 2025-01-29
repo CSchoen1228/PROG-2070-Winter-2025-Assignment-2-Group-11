5f849049feb3af781ba714c72e5882a3fe741f14
CSchoen1228 <cschoen1228@conestogac.on.ca>
2025-01-29 17:27:36 -0500

    Fixed the Project

--- PROG-2070-W2025-A2-G11/PROG-2070-W2025-A2-G11/Program.cs
+++ PROG-2070-W2025-A2-G11/PROG-2070-W2025-A2-G11/Program.cs
@@ -1,15 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PROG_2070_W2025_A2_G11
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
