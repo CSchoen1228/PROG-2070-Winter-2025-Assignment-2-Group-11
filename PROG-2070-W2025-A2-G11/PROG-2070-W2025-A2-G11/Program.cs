5c96da8630e382019afd500f0c1b9320a5afba7d
CSchoen1228 <cschoen1228@conestogac.on.ca>
2025-01-29 15:43:55 -0500

    Initial Commit

---
+++ PROG-2070-W2025-A2-G11/PROG-2070-W2025-A2-G11/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG_2070_W2025_A2_G11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
