a95a91fb4bc7d71978e38b826dc4f4744e117d73
CSchoen1228 <cschoen1228@conestogac.on.ca>
2025-01-29 17:48:49 -0500

    Added Product Id Constraint

--- PROG-2070-W2025-A2-G11/PROG-2070-W2025-A2-G11/Product.cs
+++ PROG-2070-W2025-A2-G11/PROG-2070-W2025-A2-G11/Product.cs
@@ -15,6 +15,10 @@ namespace PROG_2070_W2025_A2_G11
 
         public Product(int ProdID, string ProdName, decimal ItemPrice, int StockAmount)
         {
+            if( ProdID <5 || ProdID>50000)
+            {
+                throw new Exception(message:"Id out of Range");
+            }
             this.ProdID = ProdID;
             this.ProdName = ProdName;
             this.ItemPrice = ItemPrice;
