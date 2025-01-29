1938670ebf7f485aa2dac455c358bda4c48d89cf
CSchoen1228 <cschoen1228@conestogac.on.ca>
2025-01-29 16:31:54 -0500

    Implemented Product Class

---
+++ PROG-2070-W2025-A2-G11/PROG-2070-W2025-A2-G11/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG_2070_W2025_A2_G11
+{
+    public class Product
+    {
+        public int ProdID { get; set; }
+        public string ProdName { get; set; }
+        public decimal ItemPrice { get; set; }
+        public int StockAmount { get; set; }
+
+        public Product(int ProdID, string ProdName, decimal ItemPrice, int StockAmount)
+        {
+            this.ProdID = ProdID;
+            this.ProdName= ProdName;
+            this.ItemPrice = ItemPrice;
+            this.StockAmount = StockAmount;
+        }
+
+        public void Increase(int amount)
+        {
+            StockAmount += amount;
+        }
+
+        public void Decrease(int amount)
+        {
+            StockAmount -= amount;
+        }
+
+    }
+}
